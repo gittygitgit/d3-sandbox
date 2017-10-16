d1511663d32a3bfead84d90e3f2fdc5566ebf3e0
Michael Grudkowski <mgrudkowski@gmail.com>
2017-10-16 10:33:26 -0400

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+"use strict"
+ 
+import ReactDOM from 'react-dom';
+import React from 'react';
+import SimpleTest from './SimpleTest';
+
+ReactDOM.render(
+  <SimpleTest/>,
+  document.getElementById('app'));
+
